0e643524bafbba1c0bc9ed3dccc1bda8123cef86
Luke McDonald <thelukemcdonald@gmail.com>
2022-12-29 08:59:13 -0600

    fix: add support for astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bbcmonticello.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+  site: 'https://bbcmonticello.com',
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
 });
