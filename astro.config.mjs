2e7e25a759ea4296528f58d603a608de8d0305f4
Luke McDonald <thelukemcdonald@gmail.com>
2023-09-02 05:20:22 -0500

    fix: upgrade to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -8,9 +7,6 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://bbcmonticello.com/',
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
     mdx(),
     sitemap(),
     tailwind(),
