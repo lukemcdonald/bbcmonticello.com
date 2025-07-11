3e6c8afce84034adcdba633721704412a02da040
Luke McDonald <lukemcdonald@users.noreply.github.com>
2025-07-11 05:34:11 -0500

    fix: resolve content and technical issues across site (#22)

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Berean Bible Church'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION =
+  'Berean Bible Church - A family of believers in Jesus Christ seeking to honor God through His Word and sharing the Gospel in Monticello, Iowa.'
