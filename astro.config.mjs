a6d2ead0c1e8db48d73234ce497b25032dff9d60
Luke McDonald <lukemcdonald@users.noreply.github.com>
2025-08-12 06:03:13 -0500

    refactor: update packages and switch imports to use @/* alias (#25)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,5 @@
+// @ts-check
+import netlify from '@astrojs/netlify'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -5,10 +7,13 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  build: { format: 'file' },
+  image: {
+    layout: 'constrained',
+    responsiveStyles: false,
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
   site: 'https://bbcmonticello.com/',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-  ],
+  trailingSlash: 'never',
 })
